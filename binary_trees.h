eab6daf8711c515edf53713bec50d0208f0b0491
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-08-01 19:59:06 +0200

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -78,7 +78,6 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
-
 /* Helper Functions */
 void binary_tree_print(const binary_tree_t *);
 
