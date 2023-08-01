cf86d6635575454cd507ecf313854d23e319492b
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-08-01 15:18:05 +0100

    changing approach

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -19,7 +19,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	return (1 + (left_height > right_height ? left_height : right_height));
 }
 
-
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
@@ -28,18 +27,23 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int left_height = 0, right_height = 0;
+
 	if (!tree)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	if (left_height == right_height)
-	{
-		int left_perfect = binary_tree_is_perfect(tree->left);
-		int right_perfect = binary_tree_is_perfect(tree->right);
+	if (left_height != right_height)
+		return (0);
 
-		return (left_perfect && right_perfect);
-	}
+	if (!tree->left && !tree->right)
+		return (1);
+
+	if (binary_tree_is_perfect(tree->left) &&
+	    binary_tree_is_perfect(tree->right))
+		return (1);
+
 	return (0);
 }
